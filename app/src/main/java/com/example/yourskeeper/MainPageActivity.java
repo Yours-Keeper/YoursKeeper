43925a9503115d3b81aa76a38051076016d8a80a
junha kim <tto9011@naver.com>
2023-11-17 22:24:53 +0900

    부탁해요

--- app/src/main/java/com/example/yourskeeper/MainPageActivity.java
+++ app/src/main/java/com/example/yourskeeper/MainPageActivity.java
@@ -2,7 +2,10 @@ package com.example.yourskeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Button pleaseButton = findViewById(R.id.please_Btn);
+        pleaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI();
+            }
+        });
     }
+    private void updateUI() {
+        Intent intent = new Intent(this, PleaseMain_act.class);
+        startActivity(intent);
+    }
+
+
 }
