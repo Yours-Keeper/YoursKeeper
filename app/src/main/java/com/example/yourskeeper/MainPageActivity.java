0987be09e9a7fe4237894e3e5bf6b1eb1dd045a4
junha kim <tto9011@naver.com>
2023-11-18 22:55:21 +0900

    맡겨주세요 페이지

--- app/src/main/java/com/example/yourskeeper/MainPageActivity.java
+++ app/src/main/java/com/example/yourskeeper/MainPageActivity.java
@@ -1,8 +1,12 @@
 package com.example.yourskeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +14,27 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.naver.maps.map.LocationTrackingMode;
 
 public class MainPageActivity extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private static final int PERMISSION_REQUEST_CODE = 1000;
+    private static final String[] PERMISSIONS = {
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         Button pleaseButton = findViewById(R.id.please_Btn);
         Button storeButton = findViewById(R.id.store_Btn);
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
         pleaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
