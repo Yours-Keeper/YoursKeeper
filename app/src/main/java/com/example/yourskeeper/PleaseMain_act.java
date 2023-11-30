6c61baf946096d6c1d3b0801d47271475e1a1ba2
kimjunha <tto9011@naver.com>
2023-11-30 15:16:40 +0900

    했다

--- app/src/main/java/com/example/yourskeeper/PleaseMain_act.java
+++ app/src/main/java/com/example/yourskeeper/PleaseMain_act.java
@@ -4,10 +4,12 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayoutStates;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -18,73 +20,43 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.TotpSecret;
+
+import com.google.firebase.firestore.DocumentReference;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.CircleOverlay;
-import com.naver.maps.map.overlay.InfoWindow;
+
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentSnapshot;
 
-import static android.content.ContentValues.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentManager;
 
-import android.Manifest;
-import android.app.Dialog;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.location.Location;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Button;
-
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.overlay.CircleOverlay;
-import com.naver.maps.map.overlay.Marker;
-import com.naver.maps.map.util.FusedLocationSource;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.DocumentSnapshot;
-
-import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -97,7 +69,8 @@ public class PleaseMain_act extends AppCompatActivity
     private FusedLocationSource locationSource;
     private NaverMap mNaverMap;
     private double lat, lon;
-    private String userNICK="주나준";
+    private AlertDialog customDialog;
+
     private static final int PERMISSION_REQUEST_CODE = 1000;
 
 
@@ -122,7 +95,11 @@ public class PleaseMain_act extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_please_main);
         // Firebase 인증 객체 초기화
+        ImageView btnMenu = findViewById(R.id.btnMenu);
+        ImageView btnBack = findViewById(R.id.btnBack);
         mAuth = FirebaseAuth.getInstance();
+        TextView textTitle = findViewById(R.id.toolbar_Titie);
+        textTitle.setText("내 근처의 Keeper");
 
         // Firebase Firestore 객체 초기화
         db = FirebaseFirestore.getInstance();
@@ -135,6 +112,18 @@ public class PleaseMain_act extends AppCompatActivity
                 goPleaseList();
             }
         });
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { showMenuPopup(v); }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         // 위 코드 아래에 Firebase 사용자 정보 가져오기
         currentUser = mAuth.getCurrentUser();
 
@@ -200,25 +189,7 @@ public class PleaseMain_act extends AppCompatActivity
 
 
                 if (userId != null) {
-                    db.collection("storeContent").document(userId).get()
-                            .addOnCompleteListener(task -> {
-                                if (task.isSuccessful()) {
-                                    DocumentSnapshot document = task.getResult();
-                                    if (document != null && document.exists()) {
-                                     userNICK = document.getString("nickname");
 
-                                    } else {
-                                        // Firestore 문서가 없거나 null인 경우
-                                        Log.d(TAG, "문서 존재하지 않음");
-                                    }
-                                } else {
-                                    // 작업이 예외와 함께 실패한 경우
-                                    Exception exception = task.getException();
-                                    if (exception != null) {
-                                        Log.e(TAG, "문서 가져오기 오류", exception);
-                                    }
-                                }
-                            });
                     db.collection("storeContent").get()
                             .addOnCompleteListener(task -> {
                                 if (task.isSuccessful()) {
@@ -231,8 +202,8 @@ public class PleaseMain_act extends AppCompatActivity
                                             String content = document.getString("content");
                                             long point = document.getLong("point");
 
+                                            // 닉네임이 userNICK과 같지 않은 경우에만 마커 추가
 
-                                            if(!userNICK.equals(nickName)){
                                                 Marker marker = new Marker();
                                                 marker.setPosition(new LatLng(userLat, userLon));
                                                 marker.setMap(naverMap);
@@ -242,15 +213,15 @@ public class PleaseMain_act extends AppCompatActivity
                                                 }});
                                                 data.put("distance", distanceToMarker);
 
-                                                // Update distance in Firestore
+                                                // Firestore에서 거리 업데이트
                                                 db.collection("storeContent").document(document.getId()).update(data);
 
-                                                // Handle click event
+                                                // 클릭 이벤트 처리
                                                 marker.setOnClickListener(overlay -> {
-                                                    showCustomModal("Marker information", nickName, distanceToMarker, userId, userTime, content, point);
+                                                    showCustomModal("마커 정보", nickName, distanceToMarker, userId, userTime, content, point);
                                                     return true;
                                                 });
-                                            }
+
                                             // Add a marker for each document in the "storeContent" collection
 
 
@@ -377,4 +348,96 @@ public class PleaseMain_act extends AppCompatActivity
         startActivity(intent);
 
     }
+    private void showMenuPopup(View anchorView) {
+        View popupView = LayoutInflater.from(this).inflate(R.layout.menu, null);
+        int width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // 팝업 내 TextView 요소에 대한 참조 가져오기
+        TextView nicknameTextView = popupView.findViewById(R.id.nickname);
+        TextView reliabilityPointTextView = popupView.findViewById(R.id.reliability_point);
+        TextView logoutTextView = popupView.findViewById(R.id.menu_signout);
+
+        // Firestore에서 사용자 데이터를 가져와 TextView에 값 설정
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            DocumentReference userDocRef = db.collection("users").document(userId);
+
+            userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // 사용자 데이터 가져오기 및 TextView에 값 설정
+                            String nicknames = document.getString("nickname");
+                            Long reliabilityPoint = document.getLong("reliability_point");
+
+                            if (nicknames != null) {
+                                nicknameTextView.setText(nicknames);
+                            }
+
+                            if (reliabilityPoint != null) {
+                                reliabilityPointTextView.setText(String.valueOf(reliabilityPoint) + "점");
+                            }
+                        } else {
+                            Log.d(ConstraintLayoutStates.TAG, "문서가 존재하지 않습니다.");
+                        }
+                    } else {
+                        Log.d(ConstraintLayoutStates.TAG, "데이터 가져오기 실패: ", task.getException());
+                    }
+                }
+            });
+        }
+
+        // logoutTextView에 대한 onClickListener 설정
+        logoutTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그아웃 액션 처리
+                showSignoutDialog();
+                // 필요에 따라 추가적인 로그아웃 로직을 추가할 수 있습니다.
+                // 예를 들어, 사용자를 로그인 페이지로 리디렉션할 수 있습니다.
+            }
+        });
+
+        // 팝업 창 표시
+        popupWindow.showAtLocation(popupView, Gravity.TOP | Gravity.END, 30, 100);
+    }
+    private void showSignoutDialog(){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View dialogView = inflater.inflate(R.layout.signout_dialog, null); // null 자리는 거의 null로만 씀
+
+        customDialog = new AlertDialog.Builder(PleaseMain_act.this, R.style.RoundedCornersDialog_signout)
+                .setView(dialogView)
+                .create();
+
+        customDialog.show();
+
+        ImageView back = customDialog.findViewById(R.id.sign_out_back);
+        Button signOutButton = customDialog.findViewById(R.id.sign_out_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog.dismiss();
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // db에 store 정보를 삭제해야함
+                signOut();
+            }
+        });
+    }
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
+
